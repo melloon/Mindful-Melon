b05f08f3f866d72ebbeed774f93cc36c3bcd8d14
melloon <61524591+melloon@users.noreply.github.com>
2025-11-08 12:33:07 -0600

    implement basic timer in console, still needs negative time fix

---
+++ timer.js
@@ -0,0 +1,21 @@
+let timerMinutes = 1;
+let timerSeconds = timerMinutes * 60;
+const countdownElement = document.getElementById('timer');
+
+setInterval(countdownTimer, 1000); //calls countdownTimer every second
+
+function countdownTimer() {
+    let minutes = Math.floor(timerSeconds/60);
+    let seconds = timerSeconds % 60;
+
+    if (seconds < 10) {
+        seconds = '0' + seconds; //adding a leading zero
+    }
+    //countdownElement.innerHTML = minutes + ':' + seconds; //display time
+    console.log(minutes + ':' + seconds);
+    timerSeconds--;
+    if (timerSeconds < 0){
+        console.log("Time's up!");
+        clearInterval(countdownTimer); //stops the timer
+    }
+}
