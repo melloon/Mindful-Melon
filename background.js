b4ef2cbe5fee95e43641c1a6039b43e6b3a8adbc
melloon <61524591+melloon@users.noreply.github.com>
2025-11-09 08:07:28 -0600

    removed firebase code

--- background.js
+++ background.js
@@ -1,25 +1,3 @@
-/* // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDmyf0kIpOGmjuGnCyzZwrSF6IZD6jV9Xs",
-  authDomain: "mindful-melon.firebaseapp.com",
-  projectId: "mindful-melon",
-  storageBucket: "mindful-melon.firebasestorage.app",
-  messagingSenderId: "939076341150",
-  appId: "1:939076341150:web:ce7c2586fffc6425f81a8f",
-  measurementId: "G-RRGNHB2N4X"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app); */
-
 let timerSeconds = 60; // 1 minute
 let timerInterval = null;
 
